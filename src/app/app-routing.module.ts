eb771a42c81f70d7dfffd2b9be8dbf5959957cbf
Aleem pasha <aleempasha1410@gmail.com>
2024-10-03 14:59:18 +0530

    Routing-Example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StudentListComponent } from './student-list/student-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
   {path:'students',component: StudentListComponent},
-  {path:'employees',component: EmployeeListComponent}
+  {path:'employees',component: EmployeeListComponent},
+  {path:'test',component: TestComponent}
 ];
 
 @NgModule({
