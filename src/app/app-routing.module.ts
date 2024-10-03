11d22b790f4e6f9ac7e39ce7a0e6075934fe8ef5
Aleem pasha <aleempasha1410@gmail.com>
2024-10-03 13:48:16 +0530

    Routing and Navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentListComponent } from './student-list/student-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'students',component: StudentListComponent},
+  {path:'employees',component: EmployeeListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+
+export class AppRoutingModule {
+
+}
